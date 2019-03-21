dc74b1eae1d2d486193f3c89a1fe48d64c365eaf
Shafira Ananda <afishafiraa@gmail.com>
2019-03-21 07:55:36 +0700

    Updates

---
+++ precache-manifest.51c4a55c0bd9a0a3bad7ab82d8397760.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/afishafiraa/afishafiraa.github.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "153c4c0936d87c736dc9",
+    "url": "/afishafiraa/afishafiraa.github.io/static/js/runtime~main.f538218e.js"
+  },
+  {
+    "revision": "4a15d8d6729a4c89beea",
+    "url": "/afishafiraa/afishafiraa.github.io/static/js/main.ff5a9da9.chunk.js"
+  },
+  {
+    "revision": "300c13e89f0e1c70496a",
+    "url": "/afishafiraa/afishafiraa.github.io/static/js/2.6f6b556b.chunk.js"
+  },
+  {
+    "revision": "4a15d8d6729a4c89beea",
+    "url": "/afishafiraa/afishafiraa.github.io/static/css/main.7e2649d0.chunk.css"
+  },
+  {
+    "revision": "0603ba63f33cfe31d78b8d6b249eda79",
+    "url": "/afishafiraa/afishafiraa.github.io/index.html"
+  }
+];
